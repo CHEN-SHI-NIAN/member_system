f722063bc63d3e6bd7d7d3e6d3b4a0bcd254cbcb
SHINIAN <tiger940083@gmail.com>
2024-10-05 20:43:46 +0800

    github 上傳版本

--- member_system.py
+++ member_system.py
@@ -1,8 +1,9 @@
 from flask import *
 import pymongo
 from pymongo.mongo_client import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
 
-uri = "mongodb+srv://root:tiger871031@mycluster.mhzco2q.mongodb.net/?retryWrites=true&w=majority&appName=myCluster"
+uri = "your mongoDB url"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
@@ -37,40 +38,37 @@ def error():
     message= request.args.get("msg","發生錯誤，請聯繫客服")
     return render_template("error.html",message=message)
 
-@app.route("/singup", methods = ["POST"])
+@app.route("/signup", methods = ["POST"])
 def singup():
     #前端接收資料
     nickname = request.form["nickname"]
     email = request.form["email"]
     password = request.form["password"]
     #檢查是否有相同email
-    result=collection.find_one({
+    result = collection.find_one({
         "email":email
     })
     if result!= None:
         return redirect("/error?msg=信箱已被使用")
-    #放資料
+    # 將密碼加密後存入資料庫
+    hashed_password = generate_password_hash(password)  # 改進：使用密碼加密
     collection.insert_one({
-        "nickname":nickname,
-        "email" :email,
-        "password" : password
+        "nickname": nickname,
+        "email": email,
+        "password": hashed_password  # 改進：儲存加密後的密碼
     })
     return redirect("/")
 
 
-@app.route("/singin",methods=["POST"])
-def singin():
+@app.route("/signin", methods=["POST"])
+def signin():
     email = request.form["email"]
     password = request.form["password"]
-    collection = db.users
-    result = collection.find_one({
-        "$and":[
-            {"email" :email},
-            {"password":password}
-        ]
-    })
-    if result == None:
+
+    result = collection.find_one({"email": email})
+    if result is None or not check_password_hash(result["password"], password):
         return redirect("/error?msg=帳號或密碼輸入錯誤")
+
     session["nickname"] = result["nickname"]
     return redirect("/member")
 
@@ -79,13 +77,20 @@ def process_data():
     data = request.json
     address = data['address']
     abi = data['abi']
-    result = f"Processed address: {address} with ABI: {abi}"
+    functions = [item['name'] for item in abi if item['type'] == 'function']
+    
+    result = f"Processed address: {address} with functions: {functions}"
 
-    return jsonify({'result': result})
+    return jsonify({'result': result, 'functions': functions})
 
-@app.route("/singout")
+@app.route("/signout")
 def singout():
-    del session["nickname"]
+    session.pop("nickname", None)
     return redirect("/")
+
+@app.route("/comingsoon")
+def comingsoon():
+    return render_template("comingsoon.html")
+
 
 app.run(port=3000)
